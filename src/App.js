0e729ca2d2c01dfc5b9efa75a7f09fa23e0719a6
redondocybak <aredondocybak@gmail.com>
2025-02-20 14:33:56 -0500

    todoapp for sunday

--- src/App.js
+++ src/App.js
@@ -1,13 +1,43 @@
 import React from 'react'
 import './App.css'
+import { Dexie } from 'dexie'
+import { useLiveQuery } from 'dexie-react-hooks'
+
+const db = new Dexie('todoapp')
+db.version(1).stores({
+  todos: '++id, task, completed, date '
+})
+
+const { todos } = db
 
 const App = () => {
+  const allItems = useLiveQuery(() => todos.toArray(), [])
+
+  const addTask = async (event) => {
+    event.preventDefault()
+    const taskField = document.querySelector('#taskInput')
+
+    await todos.add({
+      task: taskField['value'],
+      completed: false
+    })
+
+    taskField['value'] = ''
+  }
+
+  const deleteTask = async (id) => todos.delete(id)
+
+  const toggleStatus = async (id, event) => {
+    await todos.update(id, { completed: !!event.target.checked})
+  }
+
   return (
     <div className="container">
       <h3 className="teal-text center-align">Todo App</h3>
-      <form className="add-item-form">
+      <form className="add-item-form" onSubmit={addTask}>
         <input
           type="text"
+          id="taskInput"
           className="itemField"
           placeholder="What do you want to do today?"
           required
@@ -19,25 +49,27 @@ const App = () => {
 
       <div className="card white darken-1">
         <div className="card-content">
-          <div className="row">
+          {allItems?.map(({ id, completed, task }) => (
+            <div className="row" key={id}>
             <p className="col s10">
               <label>
-                <input type="checkbox" checked className="checkbox-blue" />
-                <span className="black-tex strike-text">Call John Legend</span>
+                <input 
+                  type="checkbox" 
+                  checked={completed} 
+                  className="checkbox-blue"
+                  onChange={(event) => toggleStatus(id, event)} 
+                />
+                <span className={`black-text ${completed && 'strike-text'}`}>{task}</span>
               </label>
             </p>
-            <i className="col s2 material-icons delete-button">delete</i>
+            <i 
+              onClick={() => deleteTask(id)} 
+              className="col s2 material-icons delete-button"
+            >
+              delete
+            </i>
           </div>
-
-          <div className="row">
-            <p className="col s10">
-              <label>
-                <input type="checkbox" className="checkbox-blue" />
-                <span className="black-tex">Do my laundry</span>
-              </label>
-            </p>
-            <i className="col s2 material-icons delete-button">delete</i>
-          </div>
+          ))}
         </div>
       </div>
     </div>
